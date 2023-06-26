f00e23a5ffcbf1fad48fa077053694edc7a262d6
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-06-26 15:29:03 +0630

    Search game

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
 	genre: Genre | null;
 	platform: Platform | null;
 	sortOrder: string;
+	searchText: string;
 }
 
 const App = () => {
@@ -28,7 +29,11 @@ const App = () => {
 			}}
 		>
 			<GridItem area={"nav"}>
-				<Navbar />
+				<Navbar
+					onSubmit={(searchText) =>
+						setGameQuery({ ...gameQuery, searchText })
+					}
+				/>
 			</GridItem>
 			<Show above="lg">
 				<GridItem area={"aside"} paddingX={3}>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import SwitchColorMode from "./SwitchColorMode";
 import SearchInput from "./SearchInput";
-const Navbar = () => {
+
+interface Props {
+	onSubmit: (searchText: string) => void;
+}
+const Navbar = ({ onSubmit }: Props) => {
 	return (
 		<HStack padding={"10px"}>
 			<Image src={logo} boxSize="60px" />
-			<SearchInput></SearchInput>
+			<SearchInput onSubmit={onSubmit}></SearchInput>
 			<SwitchColorMode />
 		</HStack>
 	);
